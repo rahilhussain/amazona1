ea4fc427cddac83b6e14ab04f4a0b3f5ffc1808e
Evans Kiprotich Kiplagat <evanslagat911@gmail.com>
2020-08-30 05:13:18 -0700

    ss

--- backend/config.js
+++ backend/config.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://mydb:mydb@cluster0.y5hl2.mongodb.net/mydb?retryWrites=true&w=majority',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/amazona',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
-  accessKeyId: process.env.accessKeyId || 'AKIAIA4H5SAOTI4TWZRA',
-  secretAccessKey: process.env.secretAccessKey || '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',
+  accessKeyId: process.env.accessKeyId || 'accessKeyId',
+  secretAccessKey: process.env.secretAccessKey || 'secretAccessKey',
 };
