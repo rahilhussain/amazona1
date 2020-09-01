71d77164ec00429b28621b6b5f521498ff3f1089
rahil hussain <rahilhussainp@gmail.com>
2020-09-01 17:24:14 +0530

    Initial commit

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || '',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://amazona1:07012001@cluster0.0dsh7.mongodb.net/amazona1?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || '',
